764639a20b6e225d81166b2adc879a0d039aa245
dlalth557 <557dlalth@gmail.com>
2021-08-19 23:53:11 +0900

    delete StickerView

--- App/StickerView/app/src/main/java/edu/skku/stickerview/FlipBothDirectionsEvent.java
+++
@@ -1,8 +0,0 @@
-package edu.skku.stickerview;
-
-public class FlipBothDirectionsEvent extends AbstractFlipEvent {
-
-  @Override @edu.skku.stickerview.StickerView.Flip protected int getFlipDirection() {
-    return edu.skku.stickerview.StickerView.FLIP_VERTICALLY | edu.skku.stickerview.StickerView.FLIP_HORIZONTALLY;
-  }
-}
--- App/StickerView/app/src/main/java/edu/skku/stickerview/FlipHorizontallyEvent.java
+++
@@ -1,8 +0,0 @@
-package edu.skku.stickerview;
-
-public class FlipHorizontallyEvent extends AbstractFlipEvent {
-
-  @Override @edu.skku.stickerview.StickerView.Flip protected int getFlipDirection() {
-    return edu.skku.stickerview.StickerView.FLIP_HORIZONTALLY;
-  }
-}
