ff12ff38f6f5483eff7f115d6e4c69f617b2e00a
dlalth557 <557dlalth@gmail.com>
2021-08-23 16:05:10 +0900

    remove dp option

--- App/SketchTogether/app/src/main/java/edu/skku/sketchtogether/MainActivity.java
+++ App/SketchTogether/app/src/main/java/edu/skku/sketchtogether/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     StickerView stickerView;
     FloatingActionButton suggestButton;
     FloatingActionButton deleteButton;
-    ScalableLayout imageViewLayout;
+    LinearLayout imageViewLinearLayout;
     ImageView neighborImageView1;
     ImageView neighborImageView2;
     ImageView neighborImageView3;
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         stickerView = findViewById(R.id.stickerView);
         suggestButton = findViewById(R.id.suggestButton);
         deleteButton = findViewById(R.id.deleteButton);
-        imageViewLayout = findViewById(R.id.imageViewLayout);
+        imageViewLinearLayout = findViewById(R.id.imageViewLinearLayout);
         neighborImageView1 = findViewById(R.id.neighborImageView1);
         neighborImageView2 = findViewById(R.id.neighborImageView2);
         neighborImageView3 = findViewById(R.id.neighborImageView3);
@@ -48,42 +49,46 @@ public class MainActivity extends AppCompatActivity {
                 Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView2);
                 Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView3);
                 Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView4);
-                imageViewLayout.setVisibility(View.VISIBLE);
-                neighborImageView1.setClickable(true);
-                neighborImageView2.setClickable(true);
-                neighborImageView3.setClickable(true);
-                neighborImageView4.setClickable(true);
+                imageViewLinearLayout.setVisibility(View.VISIBLE);
             }
         });
 
         neighborImageView1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { loadSticker(neighborImageView1); }
+            public void onClick(View v) {
+                loadSticker(neighborImageView1);
+                imageViewLinearLayout.setVisibility(View.INVISIBLE);
+            }
         });
 
         neighborImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { loadSticker(neighborImageView2); }
+            public void onClick(View v) {
+                loadSticker(neighborImageView2);
+                imageViewLinearLayout.setVisibility(View.INVISIBLE);
+            }
         });
 
         neighborImageView3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { loadSticker(neighborImageView3); }
+            public void onClick(View v) {
+                loadSticker(neighborImageView3);
+                imageViewLinearLayout.setVisibility(View.INVISIBLE);
+            }
         });
 
         neighborImageView4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { loadSticker(neighborImageView4); }
+            public void onClick(View v) {
+                loadSticker(neighborImageView4);
+                imageViewLinearLayout.setVisibility(View.INVISIBLE);
+            }
         });
 
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageViewLayout.setVisibility(View.INVISIBLE);
-                neighborImageView1.setClickable(false);
-                neighborImageView2.setClickable(false);
-                neighborImageView3.setClickable(false);
-                neighborImageView4.setClickable(false);
+                imageViewLinearLayout.setVisibility(View.INVISIBLE);
             }
         });
 
