7c4515cad6103a28966529984be47f91c6fb4daf
dlalth557 <557dlalth@gmail.com>
2021-09-02 23:31:27 +0900

    bluetooth... socket error...

--- App/SketchTogether/app/src/main/java/edu/skku/sketchtogether/MainActivity.java
+++ App/SketchTogether/app/src/main/java/edu/skku/sketchtogether/MainActivity.java
@@ -3,14 +3,21 @@ package edu.skku.sketchtogether;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -20,13 +27,33 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import petrov.kristiyan.colorpicker.ColorPicker;
 
 public class MainActivity extends AppCompatActivity {
 
     Context context;
+
     FrameLayout sketchLayout;
     DrawingView sketchingView;
     DrawingView coloringView;
@@ -53,6 +80,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView neighborImageView3;
     ImageView neighborImageView4;
 
+    BluetoothManager bluetoothManager;
+    BluetoothAdapter bluetoothAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    List<String> pairedDevicesList;
+    BluetoothDevice bluetoothDevice;
+    BluetoothSocket bluetoothSocket;
+    UUID bluetoothUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
     private static final float SMALL_BRUSH_SIZE = 20;
     private static final float MEDIUM_BRUSH_SIZE = 60;
     private static final float LARGE_BRUSH_SIZE = 100;
@@ -69,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
         context = this.getApplicationContext();
 
         findViewsById();
+        OpenBTSocket();
 
         finishButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,21 +114,29 @@ public class MainActivity extends AppCompatActivity {
                 builder.setPositiveButton("네", new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        sketchScreenShot = getScreenshot(sketchingView);
+                        if (isSketchFinished == true) {
+                            SendMessageByBT("Bluetooth!");
+                            Toast.makeText(getApplicationContext(), "채색이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            sketchScreenShot = getScreenshot(sketchingView);
 
-                        // 서버로 보내서 svg 변환
+                            // 로봇팔? 서버? 에 sketchScreenShot 전송
 
-                        isSketchFinished = true;
-                        brushViewLinearLayout.setVisibility(View.INVISIBLE);
-                        cursorButton.setVisibility(View.INVISIBLE);
-                        suggestButton.setVisibility(View.INVISIBLE);
-                        finishButton.setVisibility(View.GONE);
-                        penButton.setVisibility(View.GONE);
-                        colorButton.setVisibility(View.VISIBLE);
-                        brushButton.setVisibility(View.VISIBLE);
-                        coloringView.setVisibility(View.VISIBLE);
-                        coloringView.bringToFront();
-                        Toast.makeText(getApplicationContext(), "스케치가 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                            isSketchFinished = true;
+                            brushViewLinearLayout.setVisibility(View.INVISIBLE);
+                            finishButton.setVisibility(View.GONE);
+                            deleteButton.setVisibility(View.GONE);
+                            penButton.setVisibility(View.GONE);
+                            eraserButton.setVisibility(View.GONE);
+                            cursorButton.setVisibility(View.GONE);
+                            suggestButton.setVisibility(View.GONE);
+                            colorButton.setVisibility(View.VISIBLE);
+                            brushButton.setVisibility(View.VISIBLE);
+                            coloringView.setVisibility(View.VISIBLE);
+                            coloringView.bringToFront();
+                            Toast.makeText(getApplicationContext(), "스케치가 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
                 builder.setNegativeButton("아니오", new DialogInterface.OnClickListener(){
@@ -154,10 +198,6 @@ public class MainActivity extends AppCompatActivity {
                 isEraserMode = false;
                 coloringView.bringToFront();
                 coloringView.setPenMode();
-                setBrushView(smallBrushView, SMALL_BRUSH_SIZE);
-                setBrushView(mediumBrushView, MEDIUM_BRUSH_SIZE);
-                setBrushView(largeBrushView, LARGE_BRUSH_SIZE);
-                brushViewLinearLayout.setVisibility(View.VISIBLE);
             }
         });
 
@@ -240,14 +280,8 @@ public class MainActivity extends AppCompatActivity {
                 buttonLinearLayout.setVisibility(View.INVISIBLE);
                 stickerView.bringToFront();
                 croppedScreenshot = getCroppedScreenshot(sketchLayout);
-
-                // croppedScreenshot 서버 전송하는 코드
-
-                Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView1);
-                Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView2);
-                Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView3);
-                Glide.with(context).load(R.drawable.icon_heart).into(neighborImageView4);
-                imageViewLinearLayout.setVisibility(View.VISIBLE);
+                File fileimg = BitmapConvertFile(croppedScreenshot, String.valueOf(getFilesDir()) + "file.bin");
+                SendData2Server(fileimg);
             }
         });
 
@@ -312,7 +346,146 @@ public class MainActivity extends AppCompatActivity {
                 buttonLinearLayout.setVisibility(View.VISIBLE);
             }
         });
+    }
 
+    // 블루투스 연결
+    public void OpenBTSocket() {
+        bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()){
+            Toast.makeText(getApplicationContext(), "bluetoothAdapter error", Toast.LENGTH_LONG).show();
+            return;
+        }
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("장치 선택");
+
+            pairedDevicesList = new ArrayList<String>();
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDevicesList.add(device.getName());
+            }
+            final CharSequence[] items = pairedDevicesList.toArray(new CharSequence[pairedDevicesList.size()]);
+            pairedDevicesList.toArray(new CharSequence[pairedDevicesList.size()]);
+
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int item) {
+                    for(BluetoothDevice device : pairedDevices) {
+                        if (items[item].toString().equals(device.getName())) {
+                            bluetoothDevice = device;
+                            break;
+                        }
+                    }
+                    try {
+                        bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(bluetoothUUID);
+                        bluetoothSocket.connect();
+                    } catch (IOException e) {
+                        Log.e("BluetoothService", "Connect Fail");
+                        Toast.makeText(getApplicationContext(), "bluetoothSocket.connect() error", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "pairing error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // 블루투스로 텍스트 전송
+    public void SendMessageByBT(String message) {
+        OutputStream mmOutStream = null;
+        try {
+            mmOutStream = bluetoothSocket.getOutputStream();
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "bluetoothSocket.getOutputStream() error", Toast.LENGTH_LONG).show();
+        }
+
+        byte[] bytes = message.getBytes();
+        try {
+            mmOutStream.write(bytes);
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "mmOutStream.write() error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // 비트맵 -> 파일
+    public File BitmapConvertFile(Bitmap bitmap, String strFilePath) {
+        File file = new File(getFilesDir(), "file.bin") ;
+        OutputStream out = null;
+        try {
+            file.createNewFile();
+            out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    // 파일 서버 전송
+    public void SendData2Server(File file){
+        Log.d("TEST : ", "Request");
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("files", "galleryfile", RequestBody.create(MultipartBody.FORM, file))
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://blee.iptime.org:22222/haewon")
+                .post(requestBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(5, TimeUnit.MINUTES) // connect timeout
+                .writeTimeout(5, TimeUnit.MINUTES) // write timeout
+                .readTimeout(5, TimeUnit.MINUTES) // read timeout
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("TEST : ", "Response");
+                String responseData = response.body().string();
+                Log.d("TEST : ", responseData);
+                try {
+                    JSONObject json = new JSONObject(responseData);
+                    System.out.println(json);
+
+                    List<String> suggestedImageList = new ArrayList<String>();
+                    for (int i = 0; i < 4; i++) {
+                        suggestedImageList.add(i, json.getString("img" + (i + 1)));
+                    }
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Resources resources = MainActivity.this.getResources();
+                            int resourceId1 = resources.getIdentifier(suggestedImageList.get(0), "drawable", MainActivity.this.getPackageName());
+                            int resourceId2 = resources.getIdentifier(suggestedImageList.get(1), "drawable", MainActivity.this.getPackageName());
+                            int resourceId3 = resources.getIdentifier(suggestedImageList.get(2), "drawable", MainActivity.this.getPackageName());
+                            int resourceId4 = resources.getIdentifier(suggestedImageList.get(3), "drawable", MainActivity.this.getPackageName());
+
+                            Glide.with(context).load(resourceId1).into(neighborImageView1);
+                            Glide.with(context).load(resourceId2).into(neighborImageView2);
+                            Glide.with(context).load(resourceId3).into(neighborImageView3);
+                            Glide.with(context).load(resourceId4).into(neighborImageView4);
+                            imageViewLinearLayout.setVisibility(View.VISIBLE);
+                        }
+                    });
+                } catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     // 브러쉬 사이즈 표시
@@ -425,5 +598,4 @@ public class MainActivity extends AppCompatActivity {
         neighborImageView3 = findViewById(R.id.neighborImageView3);
         neighborImageView4 = findViewById(R.id.neighborImageView4);
     }
-
 }
